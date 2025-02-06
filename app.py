289fba102bd8b8f7dd6f9138de0115c242720ff2
Arvind-A-master <arv020302@gmail.com>
2025-02-06 16:01:32 +0530

    data transformation

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from src.component.data_ingestion import DataIngestion
 from src.component.data_ingestion import DataIngestionConfig
+from src.component.data_transformation import DataTransformation
+
 
 
 if __name__ =='__main__':
     data_ingestion_obj =DataIngestion()
     train_path,test_path =data_ingestion_obj.initiate_data_ingestion()
-    print(train_path)
-    print(test_path)
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_path,test_path)
+    
